09055f3112d33d56012493a790e9d00d0e836e38
Jastinder Singh Atwal <jas.atwal@capgemini.com>
2023-10-24 12:37:57 +0100

    d93f4fb8-7954-4a11-aa70-41f03a9e64ab

--- src/chat/py/quickstart.py
+++ src/chat/py/quickstart.py
@@ -3,11 +3,11 @@ import openai
 
 openai.api_type = "azure"
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT") 
-openai.api_version = "2023-05-15"
+openai.api_version = "2023-07-01-preview"
 openai.api_key = os.getenv("AZURE_OPENAI_KEY")
 
 response = openai.ChatCompletion.create(
-    engine="gpt-35-turbo", # engine = "deployment_name".
+    engine="chat", # engine = "deployment_name".
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Does Azure OpenAI support customer managed keys?"},
